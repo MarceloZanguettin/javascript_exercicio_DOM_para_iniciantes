31271f3efbd078b05ebdd866280e057d7eb848a6
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-10-22 20:57:25 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+// Retorne o url da página atual utilizando o objeto window
+const hrefPagina = window.location.href;
+console.log(hrefPagina);
+
+// Seleciona o primeiro elento da página que possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+// Retorne a largura da janela
+const largura = window.innerWidth;
+console.log(largura);
